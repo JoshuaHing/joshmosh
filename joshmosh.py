55f5201ed671f5f6bd74ea6733cfcca42fca15a4
z5117173 <z5117173@cse.unsw.edu.au>
2018-02-14 00:11:15 +1100

    Adding markdown feature for more convenient post formatting

--- joshmosh.py
+++ joshmosh.py
@@ -1,20 +1,48 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template, session, request, url_for, redirect
+from flaskext.markdown import Markdown
 import os
 import sqlite3
 
 subject_list = ['COMP1521', 'COMP1531', 'COMP3411', 'COMP3821']
+data = []
 num_posts = 14
 
 app = Flask(__name__)
+Markdown(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     conn = create_connection('joshmosh.db')
-    data = get_content(subject_list[0], conn)
+    if request.method == 'POST':
+        subject = request.form.get('subject', '')
+        #new_post_contents = markdown2.markdown(request.form.get('new_post_contents', ''))
+        new_post_contents = request.form.get('new_post_contents', '')
+        post_id = request.form.get('post_id', '')
+        
+        #print('before =', new_post_contents)
+        #new_post_contents = Markup(new_post_contents)
+        #print('after =', new_post_contents)
+        change_content(subject, new_post_contents, post_id, conn)
+        
+    for i in range(0, 4):
+        data.append(get_content(subject_list[i], conn))
+        
     return render_template('home.html', data=data, subject_list=subject_list, num_posts=num_posts)
 
+@app.route('/edit', methods=['GET', 'POST'])  
+def edit():
+    subject = request.form.get('subject', '')
+    post_id = request.form.get('post_id', '')
+    post_contents = request.form.get('post_contents', '')
+    return render_template('edit.html', subject=subject, post_id=post_id, post_contents=post_contents)
+
+def change_content(subject, new_post_contents, post_id, conn):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE {0} SET Contents = '{1}' WHERE ID = {2}".format(subject, new_post_contents, post_id))
+ 
 def get_content(subject, conn):
     with conn:
         cursor = conn.cursor()
